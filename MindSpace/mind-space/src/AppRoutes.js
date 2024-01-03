ed17bb24524e1a16ccb5a3373c039294a8ec7bd7
kstch11 <kadyrsel001@gmail.com>
2024-01-03 19:22:51 +0100

    set profile to redux

--- MindSpace/mind-space/src/AppRoutes.js
+++ MindSpace/mind-space/src/AppRoutes.js
@@ -38,6 +38,7 @@ export default function AppRoutes() {
                         {link: '/forTherapists', label: 'For therapists'},
                         {link: '/login', label: 'Log in'},
                     ] : [
+                        {link: '', label: 'Home'},
                         {link: '/logout', label: 'Log out'},
                         {link: '/clientProfile', label: 'Profile'}
                     ]}/>

aaac9f323b3d42284a9caaf7a6bf3dd865e83cf6
kstch11 <kadyrsel001@gmail.com>
2024-01-04 16:21:23 +0100

    schedule fetched v2

---
+++ MindSpace/src/main/java/com/example/mindspace/controller/ScheduleController.java
@@ -0,0 +1,29 @@
+package com.example.mindspace.controller;
+
+import com.example.mindspace.api.ScheduleResponse;
+import com.example.mindspace.service.impl.ScheduleServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/schedule")
+public class ScheduleController {
+    private final ScheduleServiceImpl scheduleService;
+
+    @Autowired
+    public ScheduleController(ScheduleServiceImpl scheduleService) {
+        this.scheduleService = scheduleService;
+    }
+
+    @GetMapping("/schedules")
+    public ResponseEntity<List<ScheduleResponse>> getSchedule() {
+        return new ResponseEntity<>(scheduleService.findAll(), HttpStatus.OK);
+    }
+}

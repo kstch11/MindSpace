8d214be6caf5c3205d807afc062da550624e6320
kstch11 <kadyrsel001@gmail.com>
2024-01-14 21:35:16 +0100

    service worker fix

--- MindSpace/mind-space/public/serviceWorker.js
+++ MindSpace/mind-space/public/serviceWorker.js
@@ -1,74 +1,57 @@
-//
-// // Files to cache
-// const VERSION = 'v1';
-// const CACHE_NAME = `period-tracker-${VERSION}`;
-// const APP_STATIC_RESOURCES = [
-//     'index.html',
-// ];
-//
-// self.addEventListener("install", (e) => {
-//     e.waitUntil((async () => {
-//             const cache = await caches.open("cacheName_identifier");
-//             cache.addAll([
-//                 '/',
-//                 '/index.html',
-//             ]);
-//         })()
-//     );
-// });
-//
-// self.addEventListener("install", (event) => {
-//     event.waitUntil(
-//         (async () => {
-//             const cache = await caches.open(CACHE_NAME);
-//             cache.addAll(APP_STATIC_RESOURCES);
-//         })(),
-//     );
-// });
-//
-// self.addEventListener("activate", (event) => {
-//     event.waitUntil(
-//         (async () => {
-//             const names = await caches.keys();
-//             await Promise.all(
-//                 names.map((name) => {
-//                     if (name !== CACHE_NAME) {
-//                         return caches.delete(name);
-//                     }
-//                 }),
-//             );
-//             await clients.claim();
-//         })(),
-//     );
-// });
-//
-// self.addEventListener("fetch", (event) => {
-//
-//     if (event.request.mode === "navigate") {
-//         event.respondWith(caches.match("/"));
-//         return;
-//     }
-//
-//     event.respondWith(
-//         (async () => {
-//             const cache = await caches.open(CACHE_NAME);
-//             const cachedResponse = await cache.match(event.request.url);
-//             if (cachedResponse) {
-//                 // Return the cached response if it's available.
-//                 return cachedResponse;
-//             }
-//             // If resource isn't in the cache, return a 404.
-//             return new Response(null, { status: 404 });
-//         })(),
-//     );
-// });
-//
-// self.addEventListener('fetch', function(event) {
-//     event.respondWith(
-//         fetch(event.request).catch(function() {
-//             self.registration.showNotification("No Internet Connection", {
-//                 body: "You are currently offline. Some features may not be available.",
-//             });
-//         })
-//     );
-// });
+// Files to cache
+const VERSION = 'v1';
+const CACHE_NAME = `period-tracker-${VERSION}`;
+const APP_STATIC_RESOURCES = [
+    'index.html',
+];
+
+self.addEventListener("install", (e) => {
+    console.log('installing service worker')
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll([
+                `/`,
+                `index.html`,
+                `static/js/bundle.js`
+            ])
+                .then(() => self.skipWaiting());
+        })
+    )
+});
+
+self.addEventListener('activate', event =>{
+    console.log('activating');
+    event.waitUntil(self.clients.claim());
+})
+
+self.addEventListener('fetch', function (event) {
+    if (navigator.onLine) {
+        let fetchRequest = event.request.clone();
+        return fetch(fetchRequest).then((
+            function (response) {
+                if (!response || response.status !== 200 || response.type !== 'basic') {
+                    return response;
+                }
+
+                let responseToCache = response.clone();
+                caches.open(CACHE_NAME)
+                    .then(function (cache) {
+                        cache.put(event.request, responseToCache);
+                    });
+
+                return response;
+            }
+        ))
+    } else {
+        event.respondWith(
+            caches.match(event.request)
+                .then(
+                    function (response) {
+                        if (response) {
+                            return response;
+                        }
+                    }
+                )
+        );
+    }
+})
